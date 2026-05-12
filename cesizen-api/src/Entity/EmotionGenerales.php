812c8534edc5c6787437758f2f3cc9e70f120ca8
arnaudvlb <arnaud.vanlerberghe0@gmail.com>
2026-05-12 21:35:32 +0200

    Chore endpoints Emotions et EmotionGenerales

--- cesizen-api/src/Entity/EmotionGenerales.php
+++ cesizen-api/src/Entity/EmotionGenerales.php
@@ -6,8 +6,11 @@ use App\Repository\EmotionGeneralesRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Metadata\ApiResource;
+
 
 #[ORM\Entity(repositoryClass: EmotionGeneralesRepository::class)]
+#[ApiResource]
 class EmotionGenerales
 {
     #[ORM\Id]
--- cesizen-api/src/Entity/Emotions.php
+++ cesizen-api/src/Entity/Emotions.php
@@ -4,8 +4,10 @@ namespace App\Entity;
 
 use App\Repository\EmotionsRepository;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Metadata\ApiResource;
 
 #[ORM\Entity(repositoryClass: EmotionsRepository::class)]
+#[ApiResource]
 class Emotions
 {
     #[ORM\Id]
