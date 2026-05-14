846341445d43ddc3af89f84ddbb60cec14439cf1
arnaudvlb <arnaud.vanlerberghe0@gmail.com>
2026-05-14 18:45:11 +0200

    Chore Ajout test unitaire endpoint /api/emotions?emotionGenerale.id=

--- cesizen-api/tests/Api/EmotionsTest.php
+++ cesizen-api/tests/Api/EmotionsTest.php
@@ -25,4 +25,35 @@ class EmotionsTest extends ApiTestCase
             $this->assertArrayHasKey('emotionGenerale', $data['member'][0]);
         }
     }
+
+    public function testGetEmotionsByEmotionGenerale(): void
+    {
+        $client = static::createClient();
+        $response = $client->request(
+            'GET',
+            '/api/emotions?emotionGenerale.id=1'
+        );
+
+        $this->assertResponseIsSuccessful();
+
+        $data = $response->toArray();
+
+        $this->assertArrayHasKey('member', $data);
+        $this->assertIsArray($data['member']);
+
+        if (!empty($data['member'])) {
+            foreach ($data['member'] as $emotion) {
+
+                $this->assertArrayHasKey(
+                    'emotionGenerale',
+                    $emotion
+                );
+
+                $this->assertEquals(
+                    '/api/emotion_generales/1',
+                    $emotion['emotionGenerale']
+                );
+            }
+        }
+    }
 }
